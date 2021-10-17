03bdd08852244ffd37d16a0301f74d43614d101d
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-18 00:00:27 +0600

    setup and header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="nav-menu">
+               <div>
+                    <h1>Prime <span>Hospital</span> Ltd</h1>
+            </div>
+            <div className="menu">
+                 <NavLink to="/home">Home</NavLink>
+                 <NavLink to="/home">Home</NavLink>
+                 <NavLink to="/home">Home</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
