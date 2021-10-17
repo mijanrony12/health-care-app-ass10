535a9756c696f8d0b9b177294efc894049328f29
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-18 01:46:24 +0600

    ambulance done

--- src/components/HomePage/Ambulane/Ambulanc.js
+++ src/components/HomePage/Ambulane/Ambulanc.js
@@ -3,15 +3,18 @@ import './Ambulance.css'
 import ambulance from '../../../image/ambulance.png'
 const Ambulanc = () => {
     return (
-        <div>
-            <h1>Emergency Ambulance Service</h1>
-            <div>
+        <div >
+            <h1 className="text-center mt-5 heading">Emergency Ambulance Service</h1>
+            <div className="ambulance-section">
+                <div>
                   <img src={ambulance} alt="" />
-            </div>
-            <div>
-                  <p>01730306187</p>
-                  <p>01730306194</p>
-                  <p>01730306188</p>
+                </div>
+                <div className="contact">
+                    <h3><i class="fas fa-user"></i>Our Driver Number</h3>
+                    <p><i class="fas fa-phone-square"></i>01730306187</p>
+                    <p><i class="fas fa-phone-square"></i>01730306194</p>
+                    <p><i class="fas fa-phone-square"></i>01730306188</p>
+                </div>
             </div>
         </div>
     );
